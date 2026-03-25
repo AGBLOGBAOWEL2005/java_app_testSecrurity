aa7d07ebf3afbfc07e3ff97406b5adc790a4f0d3
owel05 <163338884+AGBLOGBAOWEL2005@users.noreply.github.com>
2026-03-25 11:25:50 +0000

    Refactor EmployeService to add employee and calculate performance

--- src/main/java/com/ipiecoles/java/java350/service/EmployeService.java
+++ src/main/java/com/ipiecoles/java/java350/service/EmployeService.java
@@ -1,4 +1,93 @@
-public void calculPerformanceCommercial(String matricule, Long caTraite, Long objectifCa) throws EmployeException {
+package com.ipiecoles.java.java350.service;
+
+import com.ipiecoles.java.java350.exception.EmployeException;
+import com.ipiecoles.java.java350.model.Employe;
+import com.ipiecoles.java.java350.model.Entreprise;
+import com.ipiecoles.java.java350.model.NiveauEtude;
+import com.ipiecoles.java.java350.model.Poste;
+import com.ipiecoles.java.java350.repository.EmployeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.persistence.EntityExistsException;
+import java.time.LocalDate;
+
+@Service
+public class EmployeService {
+
+    @Autowired
+    private EmployeRepository employeRepository;
+
+    /**
+     * Méthode enregistrant un nouvel employé dans l'entreprise
+     *
+     * @param nom Le nom de l'employé
+     * @param prenom Le prénom de l'employé
+     * @param poste Le poste de l'employé
+     * @param niveauEtude Le niveau d'étude de l'employé
+     * @param tempsPartiel Le pourcentage d'activité en cas de temps partiel
+     *
+     * @throws EmployeException Si on arrive au bout des matricules possibles
+     * @throws EntityExistsException Si le matricule correspond à un employé existant
+     */
+    public void embaucheEmploye(String nom, String prenom, Poste poste, NiveauEtude niveauEtude, Double tempsPartiel) throws EmployeException, EntityExistsException {
+
+        //Récupération du type d'employé à partir du poste
+        String typeEmploye = poste.name().substring(0,1);
+
+        //Récupération du dernier matricule...
+        String lastMatricule = employeRepository.findLastMatricule();
+        if(lastMatricule == null){
+            lastMatricule = Entreprise.MATRICULE_INITIAL;
+        }
+        //... et incrémentation
+        Integer numeroMatricule = Integer.parseInt(lastMatricule) + 1;
+        if(numeroMatricule >= 100000){
+            throw new EmployeException("Limite des 100000 matricules atteinte !");
+        }
+        //On complète le numéro avec des 0 à gauche
+        String matricule = "00000" + numeroMatricule;
+        matricule = typeEmploye + matricule.substring(matricule.length() - 5);
+
+        //On vérifie l'existence d'un employé avec ce matricule
+        if(employeRepository.findByMatricule(matricule) != null){
+            throw new EntityExistsException("L'employé de matricule " + matricule + " existe déjà en BDD");
+        }
+
+        //Calcul du salaire
+        Double salaire = Entreprise.COEFF_SALAIRE_ETUDES.get(niveauEtude) * Entreprise.SALAIRE_BASE;
+        if(tempsPartiel != null){
+            salaire = salaire * tempsPartiel;
+        }
+        salaire = Math.round(salaire*100d)/100d;
+
+        //Création et sauvegarde en BDD de l'employé.
+        Employe employe = new Employe(nom, prenom, matricule, LocalDate.now(), salaire, Entreprise.PERFORMANCE_BASE, tempsPartiel);
+
+        employeRepository.save(employe);
+
+    }
+
+
+    /**
+     * Méthode calculant la performance d'un commercial en fonction de ses objectifs et du chiffre d'affaire traité dans l'année.
+     * Cette performance lui est affectée et sauvegardée en BDD
+     *
+     * 1 : Si le chiffre d'affaire est inférieur de plus de 20% à l'objectif fixé, le commercial retombe à la performance de base
+     * 2 : Si le chiffre d'affaire est inférieur entre 20% et 5% par rapport à l'ojectif fixé, il perd 2 de performance (dans la limite de la performance de base)
+     * 3 : Si le chiffre d'affaire est entre -5% et +5% de l'objectif fixé, la performance reste la même.
+     * 4 : Si le chiffre d'affaire est supérieur entre 5 et 20%, il gagne 1 de performance
+     * 5 : Si le chiffre d'affaire est supérieur de plus de 20%, il gagne 4 de performance
+     *
+     * Si la performance ainsi calculée est supérieure à la moyenne des performances des commerciaux, il reçoit + 1 de performance.
+     *
+     * @param matricule le matricule du commercial
+     * @param caTraite le chiffre d'affaire traité par le commercial pendant l'année
+     * @param objectifCa l'object de chiffre d'affaire qui lui a été fixé
+     *
+     * @throws EmployeException Si le matricule est null ou ne commence pas par un C
+     */
+    public void calculPerformanceCommercial(String matricule, Long caTraite, Long objectifCa) throws EmployeException {
     // Vérification des paramètres
     validateParamsPerformanceCommercial(matricule, caTraite, objectifCa);
 
@@ -50,4 +139,5 @@ private Integer calculerPerformance(Integer performanceActuelle, Long caTraite, 
     }
 
     return performance;
+    }
 }
